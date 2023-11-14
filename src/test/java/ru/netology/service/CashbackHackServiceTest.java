7810a36caeacca1699550c3f3243efed06ee2601
Gleb <g.peretolchin@gmail.com>
2023-11-14 10:51:48 +0400

    New commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(2000), 0);
+    }
+
+    @org.junit.Test
+    public void testRemainForCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertEquals(service.remain(900), 100);
+    }
+}
